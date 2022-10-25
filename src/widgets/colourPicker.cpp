dd48ad455c265ce4a88c0e74bea8354bf19c0b9b
SO9010 <so9019sami@outlook.com>
2022-10-25 16:16:26 +0100

    k

--- src/widgets/colourPicker.cpp
+++ src/widgets/colourPicker.cpp
@@ -1,16 +1,41 @@
 #include "colourPicker.h"
 
-colourPicker::colourPicker() : w(255), h(255){
+colourPicker::colourPicker(){
 }
 
 colourPicker::~colourPicker(){
 
 }
 
+
 void colourPicker::renderColourPicker(){
     container container;
-    container.addToContainer(255,255);
+    container.addToContainer(w + 16, h + 16);
     container.renderContainer();
+    int cX = x;
+    int cY = y;
+    int r = 256, g = 256, b = 256;
+    int brightness = 200;
+                g += brightness;
+            b += brightness;
+            r += brightness;
+    for(int i = 0; i < w; i++){
+        for(int j = 0; j < h; j++){
+
+            SDL_SetRenderDrawColor(window::rend, r, g, b, 255);
+            SDL_RenderDrawPoint(window::rend, cX, cY);
+            g--;
+            cX++;
+        }
+        g--;
+        r++;
+        b--;
+        cX = x;
+        cY++;            
+        //r = brightness;
+        //g = brightness;
+        //b = b * brightness;
+    }
 }
 
 void colourPicker::renderColourPickerDot(){
